15f3cc239becd6f7a522077b254f81bf6323231f
Nico Isenberg <nisenberg4@gmail.com>
2026-07-19 14:50:08 +0200

    Add tests for required column validation

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from src.validation import validate_not_empty
+from src.validation import validate_not_empty, validate_required_columns
 
 def test_validate_not_empty_with_data():
 
@@ -18,5 +18,27 @@ def test_validate_not_empty_with_empty_dataframe():
     data = pd.DataFrame()
 
     result = validate_not_empty(data)
+
+    assert result is False
+
+
+def test_validate_required_columns_with_existing_columns():
+    
+    data = pd.DataFrame({
+        "Depth": [20]
+    })
+
+    result = validate_required_columns(data, ["Depth"])
+
+    assert result is True
+
+
+def test_validate_required_columns_with_missing_columns():
+
+    data = pd.DataFrame({
+        "Temperature": [20]
+    })
+
+    result = validate_required_columns(data, ["Depth"])
 
     assert result is False
